d05b34d749d66667960a40a12ec64d29a934c791
canerkocamaz <kocamaz.caner@gmail.com>
2022-12-07 21:27:01 +0300

    Add files via upload

---
+++ UWP/UWP/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace UWP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.hakkındaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Blue;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(2, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 56);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Yazma Korumayı Etkinleştir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.ForeColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(136, 32);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 56);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Yazma Korumayı Kaldır";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hakkındaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(273, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // hakkındaToolStripMenuItem
+            // 
+            this.hakkındaToolStripMenuItem.Name = "hakkındaToolStripMenuItem";
+            this.hakkındaToolStripMenuItem.Size = new System.Drawing.Size(62, 20);
+            this.hakkındaToolStripMenuItem.Text = "Hakkında";
+            this.hakkındaToolStripMenuItem.Click += new System.EventHandler(this.hakkındaToolStripMenuItem_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Usb Yazma Koruma";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(273, 103);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Usb Yazma Koruması";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem hakkındaToolStripMenuItem;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ UWP/UWP/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace UWP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        RegistryKey key;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            key=Registry.LocalMachine.OpenSubKey("System\\CurrentControlSet\\Control\\StorageDevicePolicies");
+            if (key == null)
+            {
+                key = Registry.LocalMachine.CreateSubKey("System\\CurrentControlSet\\Control\\StorageDevicePolicies");
+            }
+            if ((int)key.GetValue("WriteProtect") == 0)
+            {
+                MessageBox.Show("Dikkat!! Usb Yazma Koruması etkin değil....", "Usb Yazma Koruması", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                button1.Enabled = true;
+                button2.Enabled = false;
+            }
+            else if ((int)key.GetValue("WriteProtect") == 1)
+            {
+                MessageBox.Show("Dikkat!! Usb Yazma Koruması zaten etkin....", "Usb Yazma Koruması", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                button1.Enabled = false;
+                button2.Enabled = true;
+            }
+           
+            
+            key.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            key = Registry.LocalMachine.OpenSubKey("System\\CurrentControlSet\\Control\\StorageDevicePolicies",true);
+            key.SetValue("WriteProtect", 1);
+            MessageBox.Show("Dikkat!! Usb Yazma Koruması etkinleştirildi....", "Usb Yazma Koruması", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            button1.Enabled = false;
+            button2.Enabled = true;
+            key.Close();
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            key = Registry.LocalMachine.OpenSubKey("System\\CurrentControlSet\\Control\\StorageDevicePolicies",true);
+            key.SetValue("WriteProtect", 0);
+            MessageBox.Show("Dikkat!! Usb Yazma Koruması kaldırıldı....", "Usb Yazma Koruması", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            button1.Enabled = true;
+            button2.Enabled = false;
+            key.Close();
+        }
+
+        private void hakkındaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bu program sayisaldelil.net tarafından hazırlanmıştır.\nHerhangi bir zarardan dolayı sayisaldelil.net sorumlu tutulamaz.\n Detaylı bilgi için www.sayisaldelil.net ");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult sonuc=MessageBox.Show("çıkmak istediğinizden emin misiniz?\nEğer çıkış yaparsanız yazma koruması kaldırılacak","Çıkış",MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (sonuc == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+            key = Registry.LocalMachine.OpenSubKey("System\\CurrentControlSet\\Control\\StorageDevicePolicies", true);
+            key.SetValue("WriteProtect", 0);
+            key.Close();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == WindowState)
+            {
+                Hide();
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+        }
+
+
+       
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
